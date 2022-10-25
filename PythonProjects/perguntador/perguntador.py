cce2f49aba909fe85ae3a6d44199adf9e16c79ad
Samuel Mendonça <guilsydev@gmail.com>
2022-10-25 18:57:59 -0300

    Atualizando perguntador.py

--- PythonProjects/perguntador/perguntador.py
+++ PythonProjects/perguntador/perguntador.py
@@ -1,28 +1,37 @@
-print("")
+#ÍNICIO
+
 nome = input("Escreva o seu nome: ")
+print("")
 print("Bem-vindo,",nome,"!")
 
 print("")
-jogando = input("Vamos começar? (s/n) ")
+jogando = input("Vamos começar? (s/n)  \n")
 
-if jogando.lower() != "s" or "sim" or "Sim":
+if jogando.lower() != "s":
     quit()
 
 print("")
-print("Ótimo!")
+print("| ÓTIMO! |")
+print("")
 pontos = 0
+
 
 #PRIMEIRA PERGUNTA
 print("")
+print("| PRIMEIRA PERGUNTA |")
+print("")
+print("PYTHON É UMA: \n")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print('1. Linguagem de Programação')
 print('2. Uma cobra')
 print('3. Streamer')
 print('4. Nenhum destes')
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print("")
-resposta=int( input('Python é uma (1, 2, 3 or 4): ') )
+resposta=int( input('PYTHON É UMA: (1, 2, 3 or 4) \n'))
 print("")
 
-if (resposta==1):
+if (resposta==1) or (resposta==2):
     print("")
     print('Correto!')
     print("")
@@ -32,9 +41,15 @@ else:
     print('Incorreto!')
     print("")
     
+
 #SEGUNDA PERGUNTA
-anwer = input("HTML é uma linguagem de progamação? (s/n) ")
-if anwer.lower() == "n" or "nao" or "Nao" or "Não":
+
+
+print("")
+print("| SEGUNDA PERGUNTA |")
+print("")
+anwer = input("HTML é uma linguagem de progamação? (s/n) \n")
+if anwer.lower() == "n":
     print("")
     print("Correto!")
     print("")
@@ -44,26 +59,39 @@ else:
     print("Incorreto!")
     print("")
 
+
 #TERCEIRA PERGUNTA
+
+
 print("")
+print("| TERCEIRA PERGUNTA |")
+print("")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print('1. Um profissional de T.I')
 print('2. Uma pessoa que mexe com Python')
-print('3. Streamer')
+print('3. Um Nerd')
 print('4. Nenhum destes')
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print("")
-resposta=int( input('O programador é (1, 2, 3 or 4): ') )
+resposta=int( input('O PROGRAMADOR É: (1, 2, 3 or 4) \n') )
 print("")
 
 if (resposta==1):
     print("")
-    print('Correto!')
+    print('Correto! (+1 PONTO)')
     print("")
     pontos += 1
 else:
     print("")
-    print('Incorreto!')
+    print('Incorreto! (-1 PONTO)')
     print("")
+    pontos -= 1
+
+
+#FINAL
+
 
 print("")
 print("Você conseguiu " + str(pontos) + " pontos!, Parabéns!")
 print("Você alcançõu a média de " + str((pontos / 4 ) * 100) + "%.")
+print("")
